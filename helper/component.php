6aa031c96bd6cd5b9bda20706d9302fb9a67458b
Júlio Pontes <juliopfneto@gmail.com>
2012-09-01 17:55:09 -0300

    no direct access

--- helper/component.php
+++ helper/component.php
@@ -1,4 +1,15 @@
 <?php
+/**
+* @package Joomla.Plugin
+* @subpackage System.joomlaoverride
+*
+* @copyright Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+* @license GNU General Public License version 2 or later; see LICENSE.txt
+*/
+ 
+// no direct access
+defined('_JEXEC') or die;
+
 jimport('joomla.filesystem.path');
 jimport('joomla.filesystem.folder');
 

bc1ec3421045e8d1e50f4d2fa817b77070045bf4
Kris Sum <Kris@switchsystems.co.uk>
2014-12-10 15:00:18 +0000

    fix for deprecated usage of split()

--- core/loader.php
+++ core/loader.php
@@ -3,10 +3,10 @@ spl_autoload_register(function($class_name){
 	$original_class = $class_name;
 	$sufix = JoomlaOverrideHelperOverride::SUFIX;
 	$prefix = JoomlaOverrideHelperOverride::PREFIX;
-	
+
 	$class_prefix = substr($class_name,0,strlen($prefix));
 	$class_sufix = substr($class_name,strlen($sufix) * -1);
-	
+
 	if (
 		(!empty($prefix) && !empty($class_prefix) && ($class_prefix != $prefix))
 		||
@@ -15,7 +15,7 @@ spl_autoload_register(function($class_name){
 	{
 		return false;
 	}
-	
+
 	//check for com_class_namedefault
 	if (!empty($prefix))
 	{
@@ -25,14 +25,14 @@ spl_autoload_register(function($class_name){
 	{
 		$class_name = substr($class_name,0,strlen($sufix) * -1);
 	}
-	
-	
+
+
 	$class_name = strtolower($class_name);
 	if (strpos($class_name,'controller') !== false)
 	{
 		$format = JFactory::getApplication()->input->get('format');
-		$parts = split('controller',$class_name);
-		
+		$parts = explode('controller',$class_name);
+
 		$file = '/components/com_'.strtolower($parts[0]);
 		if (count($parts) > 1)
 		{
@@ -46,7 +46,7 @@ spl_autoload_register(function($class_name){
 		else {
 			$file .= '/controllers.php';
 		}
-		
+
 		if (is_file(JPATH_BASE.$file))
 		{
 			if (!class_exists($original_class))
@@ -57,10 +57,10 @@ spl_autoload_register(function($class_name){
 	}
 	if (strpos($class_name,'model') !== false)
 	{
-		$parts = split('model',$class_name);
-		
+		$parts = explode('model',$class_name);
+
 		$file = '/components/com_'.strtolower($parts[0]).'/models/'.strtolower(end($parts)).'.php';
-		
+
 		if (is_file(JPATH_BASE.$file))
 		{
 			if (!class_exists($original_class))
@@ -72,13 +72,13 @@ spl_autoload_register(function($class_name){
 	if (strpos($class_name,'view') !== false)
 	{
 		$format = JFactory::getApplication()->input->get('format','html');
-		$parts = split('view',$class_name);
-		
+		$parts = explode('view',$class_name);
+
 		$file = '/components/com_'.strtolower($parts[0]).'/views/'.strtolower(end($parts)).'/view.'.$format.'.php';
-		
+
 		if (is_file(JPATH_BASE.$file))
 		{
-			
+
 			if (!class_exists($original_class))
 			{
 				JoomlaOverrideHelperOverride::load(JoomlaOverrideHelperOverride::fixDefines(JoomlaOverrideHelperOverride::createDefaultClass(JPATH_BASE.$file)));
@@ -87,13 +87,13 @@ spl_autoload_register(function($class_name){
 	}
 	if (strpos($class_name,'table') !== false)
 	{
-		$parts = split('table',$class_name);
-		
+		$parts = explode('table',$class_name);
+
 		$file = '/components/com_'.strtolower($parts[0]).'/tables/'.strtolower(end($parts)).'.php';
-		
+
 		if (is_file(JPATH_BASE.$file))
 		{
-			
+
 			if (!class_exists($original_class))
 			{
 				JoomlaOverrideHelperOverride::load(JoomlaOverrideHelperOverride::fixDefines(JoomlaOverrideHelperOverride::createDefaultClass(JPATH_BASE.$file)));
